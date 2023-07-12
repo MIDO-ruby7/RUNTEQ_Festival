e6e9211ffa14077294871e39a73bd160fe23670e
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-12 11:32:15 +0900

    ⌨️ rails d model oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'static_pages#home'
+  post 'oauth/callback', to: 'oauths#callback'
+  get 'oauth/callback', to: 'oauths#callback'
+  get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
+  get 'oauths/secret_word', to: 'oauths#secret_word'
+  post 'oauths/secret_word', to: 'oauths#secret_word'
+  get 'login', to: 'oauths#login', :as => :login
+  delete 'logout' => 'oauths#destroy', :as => :logout
 end
