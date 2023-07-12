55b766c6d41d0223e52c91bc0d4297b8c51a50f4
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-12 10:42:50 +0900

    ✨ADD:name, email ,avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :avatar, presence: true
+  validates :provider, presence: true
+  validates :uid, presence: true
+  validates :user_id, presence: true, uniqueness: true
 end
