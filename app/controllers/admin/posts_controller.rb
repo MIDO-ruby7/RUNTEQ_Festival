e35674eeee95f7877dd590768ec6a7f2e80c19e8
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-19 11:08:26 +0900

    🐛FIX:リファクタリング、avatarを一旦削除

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,14 +1,31 @@
-class Admin::PostsController < ApplicationController
+class Admin::PostsController < Admin::BaseController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find_by(id: params[:id])
+  def show; end
+
+  def edit; end
+
+  def update
+    if @post = Post.update(post_params)
+      redirect_to admin_post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
+
 
   def import
     Admin.import(params[:file])
     redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,25 +1,27 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end
 
-  def edit
-    @user = User.find_by(id: params[:id])
-  end
+  def edit; end
 
   def update
-    user = User.find_by(id: params[:id])
-    user.update(user_params)
+    @user.update(user_params)
     redirect_to admin_users_path
   end
 
   def destroy
-    user = User.find_by(id: params[:id])
-    user.destroy
+    @user.destroy
     redirect_to admin_users_path
   end
 
   private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :avatar, :graduating_class, :role)
