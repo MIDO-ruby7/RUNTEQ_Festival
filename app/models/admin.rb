7e1db87baa6245dd7181dfe53c78859cdb5b32cd
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-21 08:37:30 +0900

    🐛FIX:第1回が読み込めるようにバリデーションを調整、postgresqlのidとぶつかるエラーを修正

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,14 +1,13 @@
 class Admin < ApplicationRecord
   def self.import(file, category_id)
-    updatable_attributes = ["id", "graduating_class", "name", "app_name", "contact", "app_url", "github_url", "usage_technology", "points_for_effort", "target_users", "pain_point", "remedy", "comment"]
+    updatable_attributes = ["graduating_class", "name", "app_name", "contact", "app_url", "github_url", "usage_technology", "points_for_effort", "target_users", "pain_point", "remedy", "comment"]
 
     CSV.foreach(file.path, headers: true) do |row|
       # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
-      post = Post.find_by(id: row["id"]) || Post.new
-      # CSVからデータを取得し、設定する
-      post.attributes = row.to_hash.slice(*updatable_attributes)
-      post.category_id = category_id
-      post.save
+      post = Post.create_or_find_by(id: row["id"]) do |p|
+        p.attributes = row.to_hash.slice(*updatable_attributes)
+        p.category_id = category_id
+      end
     end
   end
 end
