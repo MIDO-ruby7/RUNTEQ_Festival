6ea08eba73d1daed503ad3bda5155d426bb17b46
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-20 10:10:33 +0900

    ✨ADD:flash message

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -11,7 +11,7 @@ class OauthsController < ApplicationController
     provider = params[:provider]
     client = Octokit::Client.new(:access_token => ENV['GITHUB_PERSONAL_ACCESS_TOKEN'])
     if (@user = login_from(provider)) && client.organization_member?('runteq', @user.name )
-      redirect_to posts_path,  success: t('.success')
+      redirect_to posts_path,  notice: t('.success')
     else
       begin
         # 特定の組織にユーザーが所属しているかどうかを確認する
@@ -19,18 +19,18 @@ class OauthsController < ApplicationController
         if client.organization_member?('runteq', @user.name )
           reset_session
           auto_login(@user)
-          redirect_to posts_path,  success: t('.success')
+          redirect_to posts_path,  notice: t('.success')
         else
-          redirect_to root_path, danger: t('.no_organization')
+          redirect_to root_path, alert: t('.no_organization')
         end
       rescue
-        redirect_to root_path, danger: t('.fail')
+        redirect_to root_path, alert: t('.fail')
       end
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, success: t('.success')
+    redirect_to root_path, notice: t('.success')
   end
 end
