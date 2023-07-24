e168747a57a6b77197f3f0375479a9c8836622ab
Midori Takahashi <mintgreen7programming@gmail.com>
2023-07-24 12:55:48 +0900

    FIX:不要なファイルを削除

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# 自分のアプリに合わせて必要なコマンドを修正してください
-bin/rails db:create
-bundle exec ridgepole --config config/database.yml --env development --file db/Schemafile --apply
-#bin/rails db:seed　←必要なければコメントアウト
-
-rm -f tmp/pids/server.pid && bin/rails s
